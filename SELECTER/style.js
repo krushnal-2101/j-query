627954fb4fff5d10fce36f1ee3c269cacb4fd77e
Krushnal Bhatti <krushnalbhatti71@gmail.com>
2025-09-09 13:10:07 +0530

    Add files via upload

---
+++ SELECTER/style.js
@@ -0,0 +1,16 @@
+
+
+$("*").css({
+    "background-color":"black",
+    "color":"white",
+});
+
+$("div>p").css("color", "green",);
+
+$("ul>li").css("color", "blue");
+
+$(".class").hide()
+
+$("#id").css("font-size", "30px")
+
+$("[href]").css("text-decoration", "none")
